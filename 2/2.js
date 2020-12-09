bd1ffd5c577a7b08fb929af3003ea037fdbc93d9
Thomas Qvidahl <thomas.qvidahl@evry.com>
2020-12-09 19:34:14 +0100

    Day 2 pt 2 done.

--- 2/2.js
+++ 2/2.js
@@ -1,11 +1,17 @@
-function day2() {
+function getInput(file) {
     var fs = require('fs');
 
-    var array = fs.readFileSync('./2/2.txt').toString().split("\n");
-    var validPasswords = 0;
-    //var array = ["2-5 z: zzztvz", "1-3 g fdsgyt"];
+    return fs.readFileSync(file).toString().split("\n");
+}
+function day2() {
+    
+    var array = getInput("./2/2.txt")
+    var validPasswords1 = 0;
+    var validPasswords2 = 0;
+    
+   //var array = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
 
-    occurrences = function (pass, lett) {
+    occurrences1 = function (pass, lett) {
         var count = 0;
         for (var i = 0; i < pass.length; i++) {
             if (lett == pass[i]) {
@@ -14,6 +20,16 @@ function day2() {
         };
         return count;
     };
+
+    occurrences2 = function (pass, lett, min, max) {
+    
+        var minMatch = pass[min-1] == lett;
+        var maxMatch = pass[max-1] == lett;
+        var valid = (minMatch && !maxMatch) || (!minMatch && maxMatch);
+        console.log(pass, lett, minmax, min, max, minMatch, maxMatch, valid);
+        return valid
+    }
+
     for (i in array) {
         var elements = array[i].split(" ");
         var range = elements[0]; // 2-5
@@ -25,17 +41,24 @@ function day2() {
         var min = parseInt(minmax[0]);
         var max = parseInt(minmax[1]);
 
-        var numberOfmatches = occurrences(password, letter);
+        var numberOfmatches1 = occurrences1(password, letter);
+        
+        if (numberOfmatches1 >= min && numberOfmatches1 <= max) {
+            validPasswords1++;
+        }
 
-        if (numberOfmatches >= min && numberOfmatches <= max) {
-            validPasswords++;
+        if (occurrences2(password, letter, min, max)) {
+            validPasswords2++;
         }
     }
-    return "Part 1; " + validPasswords;
+
+    return "Part 1; " + validPasswords1 + "\n" + "Part 2; " + validPasswords2;
 }
 
-function d2p2() {
-    
+function d2p2(password, letter) {
+
+
+    return isValid;
 }
 
 module.exports = {
