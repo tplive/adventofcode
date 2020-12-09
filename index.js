62e4511043476f1cd87ff941dd3df9ca8d555579
Thomas Qvidahl <thomas.qvidahl@evry.com>
2020-12-09 17:00:20 +0100

    Day1; more better.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const chalk = require("chalk");
 const boxen = require("boxen");
 const day1 = require("./1/1");
 
-const greeting = chalk.white.bold("Results for Day 1");
+var greeting = chalk.white.bold("Results for Day 1:\n");
 
 const boxenOptions = {
  padding: 1,
@@ -15,6 +15,7 @@ const boxenOptions = {
  borderColor: "green",
  backgroundColor: "#555555"
 };
+greeting += day1.day1();
 console.log(boxen( greeting, boxenOptions ));
-console.log(boxen( day1.day1(), boxenOptions ));
+
 
